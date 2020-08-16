92411e5861c437b190e79c7e1e55baff3e191a2c
itdaniher <itdaniher@gmail.com>
2020-08-16 00:37:06 -0400

    black up

--- uftpd.py
+++ uftpd.py
@@ -38,39 +38,49 @@ client_list = []
 verbose_l = 0
 client_busy = False
 # Interfaces: (IP-Address (string), IP-Address (integer), Netmask (integer))
-AP_addr = ("0.0.0.0", 0, 0xffffff00)
-STA_addr = ("0.0.0.0", 0, 0xffffff00)
+AP_addr = ("0.0.0.0", 0, 0xFFFFFF00)
+STA_addr = ("0.0.0.0", 0, 0xFFFFFF00)
 
-_month_name = ("", "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-               "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+_month_name = (
+    "",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+)
 
 
 class FTP_client:
-
     def __init__(self, ftpsocket):
         global AP_addr, STA_addr
         self.command_client, self.remote_addr = ftpsocket.accept()
         self.remote_addr = self.remote_addr[0]
         self.command_client.settimeout(_COMMAND_TIMEOUT)
         log_msg(1, "FTP Command connection from:", self.remote_addr)
-        self.command_client.setsockopt(socket.SOL_SOCKET,
-                                       _SO_REGISTER_HANDLER,
-                                       self.exec_ftp_command)
+        self.command_client.setsockopt(
+            socket.SOL_SOCKET, _SO_REGISTER_HANDLER, self.exec_ftp_command
+        )
         self.command_client.sendall("220 Hello, this is the ESP8266.\r\n")
-        self.cwd = '/'
+        self.cwd = "/"
         self.fromname = None
-#        self.logged_in = False
+        #        self.logged_in = False
         self.act_data_addr = self.remote_addr
         self.DATA_PORT = 20
         self.active = True
         # check which interface was used by comparing the caller's ip
         # adress with the ip adresses of STA and AP; consider netmask;
         # select IP address for passive mode
-        if ((AP_addr[1] & AP_addr[2]) ==
-           (num_ip(self.remote_addr) & AP_addr[2])):
+        if (AP_addr[1] & AP_addr[2]) == (num_ip(self.remote_addr) & AP_addr[2]):
             self.pasv_data_addr = AP_addr[0]
-        elif ((STA_addr[1] & STA_addr[2]) ==
-              (num_ip(self.remote_addr) & STA_addr[2])):
+        elif (STA_addr[1] & STA_addr[2]) == (num_ip(self.remote_addr) & STA_addr[2]):
             self.pasv_data_addr = STA_addr[0]
         else:
             self.pasv_data_addr = "0.0.0.0"  # Ivalid value
@@ -84,8 +94,7 @@ class FTP_client:
             try:
                 for fname in uos.listdir(path):
                     if self.fncmp(fname, pattern):
-                        data_client.sendall(
-                            self.make_description(path, fname, full))
+                        data_client.sendall(self.make_description(path, fname, full))
             except:
                 pass
 
@@ -93,19 +102,25 @@ class FTP_client:
         global _month_name
         if full:
             stat = uos.stat(self.get_absolute_path(path, fname))
-            file_permissions = ("drwxr-xr-x"
-                                if (stat[0] & 0o170000 == 0o040000)
-                                else "-rw-r--r--")
+            file_permissions = (
+                "drwxr-xr-x" if (stat[0] & 0o170000 == 0o040000) else "-rw-r--r--"
+            )
             file_size = stat[6]
             tm = localtime(stat[7])
             if tm[0] != localtime()[0]:
-                description = "{} 1 owner group {:>10} {} {:2} {:>5} {}\r\n".\
-                    format(file_permissions, file_size,
-                           _month_name[tm[1]], tm[2], tm[0], fname)
+                description = "{} 1 owner group {:>10} {} {:2} {:>5} {}\r\n".format(
+                    file_permissions, file_size, _month_name[tm[1]], tm[2], tm[0], fname
+                )
             else:
-                description = "{} 1 owner group {:>10} {} {:2} {:02}:{:02} {}\r\n".\
-                    format(file_permissions, file_size,
-                           _month_name[tm[1]], tm[2], tm[3], tm[4], fname)
+                description = "{} 1 owner group {:>10} {} {:2} {:02}:{:02} {}\r\n".format(
+                    file_permissions,
+                    file_size,
+                    _month_name[tm[1]],
+                    tm[2],
+                    tm[3],
+                    tm[4],
+                    fname,
+                )
         else:
             description = fname + "\r\n"
         return description
@@ -130,22 +145,22 @@ class FTP_client:
         # Just a few special cases "..", "." and ""
         # If payload start's with /, set cwd to /
         # and consider the remainder a relative path
-        if payload.startswith('/'):
+        if payload.startswith("/"):
             cwd = "/"
         for token in payload.split("/"):
-            if token == '..':
+            if token == "..":
                 cwd = self.split_path(cwd)[0]
-            elif token != '.' and token != '':
-                if cwd == '/':
+            elif token != "." and token != "":
+                if cwd == "/":
                     cwd += token
                 else:
-                    cwd = cwd + '/' + token
+                    cwd = cwd + "/" + token
         return cwd
 
     def split_path(self, path):  # instead of path.rpartition('/')
-        tail = path.split('/')[-1]
-        head = path[:-(len(tail) + 1)]
-        return ('/' if head == '' else head, tail)
+        tail = path.split("/")[-1]
+        head = path[: -(len(tail) + 1)]
+        return ("/" if head == "" else head, tail)
 
     # compare fname against pattern. Pattern may contain
     # the wildcards ? and *.
@@ -153,15 +168,15 @@ class FTP_client:
         pi = 0
         si = 0
         while pi < len(pattern) and si < len(fname):
-            if (fname[si] == pattern[pi]) or (pattern[pi] == '?'):
+            if (fname[si] == pattern[pi]) or (pattern[pi] == "?"):
                 si += 1
                 pi += 1
             else:
-                if pattern[pi] == '*':  # recurse
+                if pattern[pi] == "*":  # recurse
                     if pi == len(pattern.rstrip("*?")):  # only wildcards left
                         return True
                     while si < len(fname):
-                        if self.fncmp(fname[si:], pattern[pi + 1:]):
+                        if self.fncmp(fname[si:], pattern[pi + 1 :]):
                             return True
                         else:
                             si += 1
@@ -215,7 +230,7 @@ class FTP_client:
             #    return
 
             command = data.split()[0].upper()
-            payload = data[len(command):].lstrip()  # partition is missing
+            payload = data[len(command) :].lstrip()  # partition is missing
             path = self.get_absolute_path(self.cwd, payload)
             log_msg(1, "Command={}, Payload={}".format(command, payload))
 
@@ -234,9 +249,9 @@ class FTP_client:
             elif command == "SYST":
                 cl.sendall("215 UNIX Type: L8\r\n")
             elif command in ("TYPE", "NOOP", "ABOR"):  # just accept & ignore
-                cl.sendall('200 OK\r\n')
+                cl.sendall("200 OK\r\n")
             elif command == "QUIT":
-                cl.sendall('221 Bye.\r\n')
+                cl.sendall("221 Bye.\r\n")
                 close_client(cl)
             elif command == "PWD" or command == "XPWD":
                 cl.sendall('257 "{}"\r\n'.format(self.cwd))
@@ -244,44 +259,50 @@ class FTP_client:
                 try:
                     if (uos.stat(path)[0] & 0o170000) == 0o040000:
                         self.cwd = path
-                        cl.sendall('250 OK\r\n')
+                        cl.sendall("250 OK\r\n")
                     else:
-                        cl.sendall('550 Fail\r\n')
+                        cl.sendall("550 Fail\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             elif command == "PASV":
-                cl.sendall('227 Entering Passive Mode ({},{},{}).\r\n'.format(
-                    self.pasv_data_addr.replace('.', ','),
-                    _DATA_PORT >> 8, _DATA_PORT % 256))
+                cl.sendall(
+                    "227 Entering Passive Mode ({},{},{}).\r\n".format(
+                        self.pasv_data_addr.replace(".", ","),
+                        _DATA_PORT >> 8,
+                        _DATA_PORT % 256,
+                    )
+                )
                 self.active = False
             elif command == "PORT":
                 items = payload.split(",")
                 if len(items) >= 6:
-                    self.act_data_addr = '.'.join(items[:4])
+                    self.act_data_addr = ".".join(items[:4])
                     if self.act_data_addr == "127.0.1.1":
                         # replace by command session addr
                         self.act_data_addr = self.remote_addr
                     self.DATA_PORT = int(items[4]) * 256 + int(items[5])
-                    cl.sendall('200 OK\r\n')
+                    cl.sendall("200 OK\r\n")
                     self.active = True
                 else:
-                    cl.sendall('504 Fail\r\n')
+                    cl.sendall("504 Fail\r\n")
             elif command == "LIST" or command == "NLST":
                 if payload.startswith("-"):
                     option = payload.split()[0].lower()
                     path = self.get_absolute_path(
-                            self.cwd, payload[len(option):].lstrip())
+                        self.cwd, payload[len(option) :].lstrip()
+                    )
                 else:
                     option = ""
                 try:
                     data_client = self.open_dataclient()
                     cl.sendall("150 Directory listing:\r\n")
-                    self.send_list_data(path, data_client,
-                                        command == "LIST" or 'l' in option)
+                    self.send_list_data(
+                        path, data_client, command == "LIST" or "l" in option
+                    )
                     cl.sendall("226 Done.\r\n")
                     data_client.close()
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
                     if data_client is not None:
                         data_client.close()
             elif command == "RETR":
@@ -294,43 +315,51 @@ class FTP_client:
                     data_client = None
                     cl.sendall("226 Done.\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
                     if data_client is not None:
                         data_client.close()
             elif command == "STOR" or command == "APPE":
                 try:
                     data_client = self.open_dataclient()
                     cl.sendall("150 Opened data connection.\r\n")
-                    self.save_file_data(path, data_client,
-                                        "w" if command == "STOR" else "a")
+                    self.save_file_data(
+                        path, data_client, "w" if command == "STOR" else "a"
+                    )
                     # if the next statement is reached,
                     # the data_client was closed.
                     data_client = None
                     cl.sendall("226 Done.\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
                     if data_client is not None:
                         data_client.close()
             elif command == "SIZE":
                 try:
-                    cl.sendall('213 {}\r\n'.format(uos.stat(path)[6]))
+                    cl.sendall("213 {}\r\n".format(uos.stat(path)[6]))
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             elif command == "MDTM":
                 try:
-                    tm=localtime(uos.stat(path)[8])
-                    cl.sendall('213 {:04d}{:02d}{:02d}{:02d}{:02d}{:02d}\r\n'.format(*tm[0:6]))
+                    tm = localtime(uos.stat(path)[8])
+                    cl.sendall(
+                        "213 {:04d}{:02d}{:02d}{:02d}{:02d}{:02d}\r\n".format(*tm[0:6])
+                    )
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             elif command == "STAT":
                 if payload == "":
-                    cl.sendall("211-Connected to ({})\r\n"
-                               "    Data address ({})\r\n"
-                               "    TYPE: Binary STRU: File MODE: Stream\r\n"
-                               "    Session timeout {}\r\n"
-                               "211 Client count is {}\r\n".format(
-                                self.remote_addr, self.pasv_data_addr,
-                                _COMMAND_TIMEOUT, len(client_list)))
+                    cl.sendall(
+                        "211-Connected to ({})\r\n"
+                        "    Data address ({})\r\n"
+                        "    TYPE: Binary STRU: File MODE: Stream\r\n"
+                        "    Session timeout {}\r\n"
+                        "211 Client count is {}\r\n".format(
+                            self.remote_addr,
+                            self.pasv_data_addr,
+                            _COMMAND_TIMEOUT,
+                            len(client_list),
+                        )
+                    )
                 else:
                     cl.sendall("213-Directory listing:\r\n")
                     self.send_list_data(path, cl, True)
@@ -338,9 +367,9 @@ class FTP_client:
             elif command == "DELE":
                 try:
                     uos.remove(path)
-                    cl.sendall('250 OK\r\n')
+                    cl.sendall("250 OK\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             elif command == "RNFR":
                 try:
                     # just test if the name exists, exception if not
@@ -348,29 +377,29 @@ class FTP_client:
                     self.fromname = path
                     cl.sendall("350 Rename from\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             elif command == "RNTO":
-                    try:
-                        uos.rename(self.fromname, path)
-                        cl.sendall('250 OK\r\n')
-                    except:
-                        cl.sendall('550 Fail\r\n')
-                    self.fromname = None
+                try:
+                    uos.rename(self.fromname, path)
+                    cl.sendall("250 OK\r\n")
+                except:
+                    cl.sendall("550 Fail\r\n")
+                self.fromname = None
             elif command == "CDUP" or command == "XCUP":
                 self.cwd = self.get_absolute_path(self.cwd, "..")
-                cl.sendall('250 OK\r\n')
+                cl.sendall("250 OK\r\n")
             elif command == "RMD" or command == "XRMD":
                 try:
                     uos.rmdir(path)
-                    cl.sendall('250 OK\r\n')
+                    cl.sendall("250 OK\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             elif command == "MKD" or command == "XMKD":
                 try:
                     uos.mkdir(path)
-                    cl.sendall('250 OK\r\n')
+                    cl.sendall("250 OK\r\n")
                 except:
-                    cl.sendall('550 Fail\r\n')
+                    cl.sendall("550 Fail\r\n")
             else:
                 cl.sendall("502 Unsupported command.\r\n")
                 # log_msg(2,
@@ -415,8 +444,9 @@ def accept_ftp_connect(ftpsocket):
 
 def num_ip(ip):
     items = ip.split(".")
-    return (int(items[0]) << 24 | int(items[1]) << 16 |
-            int(items[2]) << 8 | int(items[3]))
+    return (
+        int(items[0]) << 24 | int(items[1]) << 16 | int(items[2]) << 8 | int(items[3])
+    )
 
 
 def stop():
@@ -425,8 +455,7 @@ def stop():
     global client_busy
 
     for client in client_list:
-        client.command_client.setsockopt(socket.SOL_SOCKET,
-                                         _SO_REGISTER_HANDLER, None)
+        client.command_client.setsockopt(socket.SOL_SOCKET, _SO_REGISTER_HANDLER, None)
         client.command_client.close()
     del client_list
     client_list = []
@@ -457,15 +486,14 @@ def start(port=21, verbose=0, splash=True):
     ftpsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     datasocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    ftpsocket.bind(('0.0.0.0', port))
-    datasocket.bind(('0.0.0.0', _DATA_PORT))
+    ftpsocket.bind(("0.0.0.0", port))
+    datasocket.bind(("0.0.0.0", _DATA_PORT))
 
     ftpsocket.listen(0)
     datasocket.listen(0)
 
     datasocket.settimeout(10)
-    ftpsocket.setsockopt(socket.SOL_SOCKET,
-                         _SO_REGISTER_HANDLER, accept_ftp_connect)
+    ftpsocket.setsockopt(socket.SOL_SOCKET, _SO_REGISTER_HANDLER, accept_ftp_connect)
 
     wlan = network.WLAN(network.AP_IF)
     if wlan.active():
